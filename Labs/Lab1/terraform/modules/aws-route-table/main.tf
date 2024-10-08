ea693cd47422684fe9b6238b7fde1f6384a6e3b6
DESKTOP-C70KB89\ASUS <20520735@gm.uit.edu.vn>
2024-10-08 23:12:54 +0700

    feat(route-table): add public and private route-table resource, variables and outputs

---
+++ Labs/Lab1/terraform/modules/aws-route-table/main.tf
@@ -0,0 +1,89 @@
+################################################################################
+# Public Route Table
+################################################################################
+locals {
+  len_public_subnets      = length(var.public_subnets)
+  len_private_subnets      = length(var.private_subnets)
+  num_public_route_tables = var.create_multiple_public_route_tables ? local.len_public_subnets : 1
+}
+
+resource "aws_route_table" "public" {
+  count =  local.num_public_route_tables 
+
+  vpc_id = var.vpc_id
+
+  tags = merge(
+    {
+      "Name" = var.create_multiple_public_route_tables ? format(
+        "${var.name}-${var.public_subnet_suffix}-%s",
+        element(var.azs, count.index),
+      ) : "${var.name}-${var.public_subnet_suffix}"
+    },
+    var.tags,
+    var.public_route_table_tags,
+  )
+}
+
+resource "aws_route_table_association" "public" {
+  count =  local.len_public_subnets 
+
+  subnet_id      = element(var.public_subnet_ids, count.index)
+  route_table_id = element(aws_route_table.public[*].id, var.create_multiple_public_route_tables ? count.index : 0)
+}
+
+resource "aws_route" "public_internet_gateway" {
+  count =  local.num_public_route_tables 
+
+  route_table_id         = aws_route_table.public[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = var.internet_gateway_id
+  timeouts {
+    create = "5m"
+  }
+}
+
+
+################################################################################
+# Private Route Table
+################################################################################
+# There are as many routing tables as the number of NAT gateways
+locals {
+  nat_gateway_count = var.single_nat_gateway
+}
+resource "aws_route_table" "private" {
+  count = local.nat_gateway_count 
+
+  vpc_id = var.vpc_id
+
+  tags = merge(
+    {
+      "Name" = var.single_nat_gateway ? "${var.name}-${var.private_subnet_suffix}" : format(
+        "${var.name}-${var.private_subnet_suffix}-%s",
+        element(var.azs, count.index),
+      )
+    },
+    var.tags,
+    var.private_route_table_tags,
+  )
+}
+
+resource "aws_route_table_association" "private" {
+  count =  local.len_private_subnets 
+  subnet_id = element(var.public_subnet_ids, count.index)
+  route_table_id = element(
+    aws_route_table.private[*].id,
+    var.single_nat_gateway ? 0 : count.index,
+  )
+}
+
+resource "aws_route" "private_nat_gateway" {
+  count =  local.nat_gateway_count 
+
+  route_table_id         = element(aws_route_table.private[*].id, count.index)
+  destination_cidr_block = var.nat_gateway_destination_cidr_block
+  nat_gateway_id         = element(var.nat_gateway_ids, count.index)
+
+  timeouts {
+    create = "5m"
+  }
+}
---
+++ Labs/Lab1/terraform/modules/aws-route-table/variables.tf
@@ -0,0 +1,101 @@
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
+
+variable "azs" {
+  description = "A list of availability zones names or ids in the region"
+  type        = list(string)
+  default     = []
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+  default     = ""
+}
+
+################################################################################
+# Publiс Route Table
+################################################################################
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "public_subnet_ids" {
+  description = "List of Public Subnet IDs"
+  type        = list(string)
+}
+
+variable "public_subnet_suffix" {
+  description = "Suffix to append to public subnets name"
+  type        = string
+  default     = "public"
+}
+
+
+
+variable "create_multiple_public_route_tables" {
+  description = "Indicates whether to create a separate route table for each public subnet. Default: `false`"
+  type        = bool
+  default     = false
+}
+
+variable "public_route_table_tags" {
+  description = "Additional tags for the public route tables"
+  type        = map(string)
+  default     = {}
+}
+
+################################################################################
+# Private Route Table
+################################################################################
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "private_subnet_ids" {
+  description = "List of Private Subnet IDs"
+  type        = list(string)
+}
+
+variable "private_subnet_suffix" {
+  description = "Suffix to append to private subnets name"
+  type        = string
+  default     = "private"
+}
+
+variable "nat_gateway_destination_cidr_block" {
+  description = "Used to pass a custom destination route for private NAT Gateway. If not specified, the default 0.0.0.0/0 is used as a destination route"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "single_nat_gateway" {
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
+  type        = bool
+  default     = false
+}
+
+variable "nat_gateway_ids" {
+  description = "List of NAT Gateway IDs"
+  type        = list(string)
+  default     = []
+}
+
+variable "private_route_table_tags" {
+  description = "Additional tags for the private route tables"
+  type        = map(string)
+  default     = {}
+}
