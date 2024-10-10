2cbd8b96314441e92d2ed07d89eab6564c7fa861
roosterhp <phuocvanho2004@gmail.com>
2024-10-10 19:33:16 +0700

    fix aws-route-table

--- Labs/Lab1/main.tf
+++ Labs/Lab1/main.tf
@@ -16,24 +16,46 @@ provider "aws" {
 
 # Use the VPC Module
 module "vpc" {
-   source = "./terraform/modules/aws-vpc-with-subnets"  
+  source = "./terraform/modules/aws-vpc-with-subnets"
 
-  create_vpc                     = true
-  name                           = "main_vpc"
-  cidr                           = "10.0.0.0/16"
-  azs                            = ["us-east-1a", "us-east-1b"]
+  create_vpc                        = true
+  name                              = "main_vpc"
+  cidr                              = "10.0.0.0/16"
   enable_network_address_usage_metrics = true
 
-  public_subnets                 = ["10.0.0.0/24", "10.0.1.0/24"]
-  map_public_ip_on_launch        = true
-  public_subnet_suffix           = "public"
+  azs                               = ["us-east-1a", "us-east-1b"]
+  public_subnets                    = ["10.0.0.0/24", "10.0.1.0/24"]
+  map_public_ip_on_launch           = true
+  public_subnet_suffix              = "public"
 
-  private_subnets                = ["10.0.2.0/24", "10.0.3.0/24"]
-  private_subnet_suffix          = "private"
+  private_subnets                   = ["10.0.2.0/24", "10.0.3.0/24"]
+  private_subnet_suffix             = "private"
 
-  create_igw                     = true
+  create_igw                        = true
 
   tags = {
     Environment = "Development"
   }
+  
+module "route_table" {
+  source = "./terraform/modules/aws-route-table"
+
+  vpc_id                          = module.vpc.vpc_id
+  public_subnet_ids               = module.vpc.public_subnet_ids
+  private_subnet_ids              = module.vpc.private_subnet_ids
+  create_multiple_public_route_tables = true
+  single_nat_gateway              = false
+  nat_gateway_ids                 = ["nat-0123456789abcdef0", "nat-0987654321abcdef0"]
+  internet_gateway_id             = module.vpc.internet_gateway_id
+  azs                             = ["us-east-1a", "us-east-1b"]
+  name                            = "main_vpc"
+
+  public_subnet_suffix            = "public"
+  private_subnet_suffix           = "private"
+
+  tags = {
+    Environment = "Development"
+  }
+}
+
 }
--- Labs/Lab1/terraform/modules/aws-route-table/main.tf
+++ Labs/Lab1/terraform/modules/aws-route-table/main.tf
@@ -1,22 +1,26 @@
 ################################################################################
-# Public Route Table
+# Locals
 ################################################################################
 locals {
-  len_public_subnets      = length(var.public_subnets)
-  len_private_subnets      = length(var.private_subnets)
+  len_public_subnets      = length(var.public_subnet_ids)
+  len_private_subnets     = length(var.private_subnet_ids)
   num_public_route_tables = var.create_multiple_public_route_tables ? local.len_public_subnets : 1
+  nat_gateway_count       = var.single_nat_gateway ? 1 : length(var.nat_gateway_ids)
 }
 
-resource "aws_route_table" "public" {
-  count =  local.num_public_route_tables 
+################################################################################
+# Public Route Table
+################################################################################
 
+resource "aws_route_table" "public" {
+  count  = local.num_public_route_tables
   vpc_id = var.vpc_id
 
   tags = merge(
     {
       "Name" = var.create_multiple_public_route_tables ? format(
         "${var.name}-${var.public_subnet_suffix}-%s",
-        element(var.azs, count.index),
+        element(var.azs, count.index)
       ) : "${var.name}-${var.public_subnet_suffix}"
     },
     var.tags,
@@ -25,41 +29,39 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  count =  local.len_public_subnets 
-
-  subnet_id      = element(var.public_subnet_ids, count.index)
-  route_table_id = element(aws_route_table.public[*].id, var.create_multiple_public_route_tables ? count.index : 0)
+  count        = local.len_public_subnets
+  subnet_id    = element(var.public_subnet_ids, count.index)
+  route_table_id = element(
+    aws_route_table.public[*].id,
+    var.create_multiple_public_route_tables ? count.index : 0
+  )
 }
 
 resource "aws_route" "public_internet_gateway" {
-  count =  local.num_public_route_tables 
+  count = local.num_public_route_tables
 
   route_table_id         = aws_route_table.public[count.index].id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = var.internet_gateway_id
+
   timeouts {
     create = "5m"
   }
 }
 
-
 ################################################################################
 # Private Route Table
 ################################################################################
-# There are as many routing tables as the number of NAT gateways
-locals {
-  nat_gateway_count = var.single_nat_gateway
-}
-resource "aws_route_table" "private" {
-  count = local.nat_gateway_count 
 
+resource "aws_route_table" "private" {
+  count  = local.nat_gateway_count
   vpc_id = var.vpc_id
 
   tags = merge(
     {
       "Name" = var.single_nat_gateway ? "${var.name}-${var.private_subnet_suffix}" : format(
         "${var.name}-${var.private_subnet_suffix}-%s",
-        element(var.azs, count.index),
+        element(var.azs, count.index)
       )
     },
     var.tags,
@@ -68,16 +70,16 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private" {
-  count =  local.len_private_subnets 
-  subnet_id = element(var.public_subnet_ids, count.index)
+  count        = local.len_private_subnets
+  subnet_id    = element(var.private_subnet_ids, count.index)
   route_table_id = element(
     aws_route_table.private[*].id,
-    var.single_nat_gateway ? 0 : count.index,
+    var.single_nat_gateway ? 0 : count.index
   )
 }
 
 resource "aws_route" "private_nat_gateway" {
-  count =  local.nat_gateway_count 
+  count = local.nat_gateway_count
 
   route_table_id         = element(aws_route_table.private[*].id, count.index)
   destination_cidr_block = var.nat_gateway_destination_cidr_block
